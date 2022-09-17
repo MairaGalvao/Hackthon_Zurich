01eba87967345f3944f46e33376c2a619ba3b398
mairaGalvao <mairagalvao01@gmail.com>
2022-09-17 23:26:54 +0300

    main page with start button to start ht egame

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,7 +1,36 @@
 import React, { useState } from "react";
+import App from "./App";
 
 function MainPage() {
-  return <></>;
+  const [displayGame, setDisplayGame] = useState(false);
+  const [displayMainPage, setDisplayMainPage] = useState(true);
+  function startGame() {
+    setDisplayMainPage(false);
+    setDisplayGame(true);
+  }
+
+  return (
+    <>
+      {displayMainPage && (
+        <div>
+          <h1>EVERY DROP COUNTS!</h1>
+          <div class="drop-container">
+            <div class="drop">
+              {" "}
+              <button className="btnStart" onClick={() => startGame()}>
+                START{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {displayGame && (
+        <div>
+          <App />{" "}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default MainPage;
