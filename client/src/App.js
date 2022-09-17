603133c2f77438ba05cf592be082e8815c4b0be0
mairaGalvao <mairagalvao01@gmail.com>
2022-09-17 20:17:02 +0300

    on click word chosen is sent to the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [word, setWord] = useState();
+
+  function handleChange(event) {
+    setWord(event.target.value);
+    console.log(event.target.value);
+  }
+
   const onClickSynonym = () => {
-    fetch("http://127.0.0.1:5000/synonym?input=pretty", {
+    console.log(word, "in synom");
+
+    fetch(`http://127.0.0.1:5000/synonym?input=${word}`, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -12,7 +22,8 @@ function App() {
   };
 
   const onClickRhyme = () => {
-    fetch("http://127.0.0.1:5000/rhyme?input=pretty", {
+    console.log(word, "in rhyme");
+    fetch(`http://127.0.0.1:5000/rhyme?input=${word}`, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -23,9 +34,10 @@ function App() {
 
   return (
     <>
+      <input onChange={handleChange} value={word}></input>
+      <div>{word}</div>
       <button onClick={onClickSynonym}>synonym</button>
       <button onClick={onClickRhyme}>rhyme</button>
-
     </>
   );
 }
